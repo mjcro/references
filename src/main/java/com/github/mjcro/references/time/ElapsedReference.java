a9ce11091a30c3cb8c36434afa033935d271e210
gotterdemarung <gotterdemarung@gmail.com>
2022-01-23 20:18:16 +0200

    :sparkles: Elapsed duration reference

---
+++ src/main/java/com/github/mjcro/references/time/ElapsedReference.java
@@ -0,0 +1,10 @@
+package com.github.mjcro.references.time;
+
+import java.time.Duration;
+
+public interface ElapsedReference {
+    /**
+     * @return Elapsed time.
+     */
+    Duration getElapsed();
+}
