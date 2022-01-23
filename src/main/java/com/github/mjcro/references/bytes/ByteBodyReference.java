a32196cf5c3374826980379f2b1e483d9c50d8e1
gotterdemarung <gotterdemarung@gmail.com>
2022-01-23 20:18:26 +0200

    :sparkles: Byte body reference

---
+++ src/main/java/com/github/mjcro/references/bytes/ByteBodyReference.java
@@ -0,0 +1,50 @@
+package com.github.mjcro.references.bytes;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public interface ByteBodyReference {
+    /**
+     * @return Entity body as bytes.
+     */
+    byte[] getBody();
+
+    /**
+     * @return True if body is empty.
+     */
+    default boolean isBodyEmpty() {
+        byte[] body = this.getBody();
+        return body == null || body.length == 0;
+    }
+
+    /**
+     * @return True if body not empty.
+     */
+    default boolean isBodyPresent() {
+        return !isBodyEmpty();
+    }
+
+    /**
+     * Returns entity body as string.
+     * If body bytes are null or empty, empty string will be returned.
+     *
+     * @param charset String character set.
+     * @return Entity body as string.
+     */
+    default String getBodyString(Charset charset) {
+        byte[] body = this.getBody();
+        return body == null || body.length == 0
+                ? ""
+                : (charset == null ? new String(body) : new String(body, charset));
+    }
+
+    /**
+     * Returns entity body as string.
+     * If body bytes are null or empty, empty string will be returned.
+     *
+     * @return Entity body as string in UTF-8 encoding.
+     */
+    default String getBodyString() {
+        return this.getBodyString(StandardCharsets.UTF_8);
+    }
+}
