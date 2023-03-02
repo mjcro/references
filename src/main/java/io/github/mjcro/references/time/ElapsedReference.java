1126f937ac8ca70063e61f4002633b1df9f61ed9
Gotterdemarung <gotterdemarung@gmail.com>
2023-03-02 10:12:06 +0200

    :sparkles: Elapsed reference with default getters for nanos and millis

--- src/main/java/io/github/mjcro/references/time/ElapsedReference.java
+++ src/main/java/io/github/mjcro/references/time/ElapsedReference.java
@@ -7,4 +7,18 @@ public interface ElapsedReference {
      * @return Elapsed time.
      */
     Duration getElapsed();
+
+    /**
+     * @return Elapsed time in milliseconds.
+     */
+    default long getElapsedMillis() {
+        return getElapsed().toMillis();
+    }
+
+    /**
+     * @return Elapsed time in nanoseconds.
+     */
+    default long getElapsedNanos() {
+        return getElapsed().toNanos();
+    }
 }
