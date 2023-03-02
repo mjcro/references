c3cd3dabde9c343f6dcd08efabe259dc390a55c1
Gotterdemarung <gotterdemarung@gmail.com>
2023-03-02 09:15:32 +0200

    :sparkles: Get body as Base64 string

--- src/main/java/io/github/mjcro/references/bytes/ByteBodyReference.java
+++ src/main/java/io/github/mjcro/references/bytes/ByteBodyReference.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public interface ByteBodyReference {
     /**
@@ -38,6 +39,19 @@ public interface ByteBodyReference {
         return body == null || body.length == 0
                 ? ""
                 : (charset == null ? new String(body) : new String(body, charset));
+    }
+
+    /**
+     * Returns entity body as Base64 string.
+     * If body bytes are null or empty, empty string will be returned.
+     *
+     * @return Entity body as Base64 string.
+     */
+    default String getBodyBase64() {
+        byte[] body = this.getBody();
+        return body == null || body.length == 0
+                ? ""
+                : Base64.getEncoder().encodeToString(body);
     }
 
     /**
